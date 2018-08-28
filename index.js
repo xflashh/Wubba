bed5c2fc04f25af8ee7967b40173643595a58609
xflashh <42582221+xflashh@users.noreply.github.com>
2018-08-29 00:16:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const botconfig = require("./botconfig.json");
+const botconfig = require("process.env.token");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
